b8cdbd741b817fae7b6f807d9df3c9359f783919
Marcus Luk <marcus.luk@oocl.com>
2020-11-27 19:20:44 +0800

    [SmartParkingBoy][Marcus Luk] Test: Create test case for throwing NotEnoughPositionException and NotEnoughPositionException given multiple parking lots.

--- src/test/java/com/oocl/cultivation/SmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/SmartParkingBoyTest.java
@@ -30,4 +30,55 @@ public class SmartParkingBoyTest {
         assertEquals(car, parkingLot2.fetch(ticket));
     }
 
+
+    @Test
+    public void should_throw_not_enough_position_exception_when_park_multiple_cars_given_two_parking_lots_are_full() throws NotEnoughPositionException {
+        //given
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(new ParkingLot(0));
+        parkingLots.add(new ParkingLot(0));
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
+
+        //when
+        final NotEnoughPositionException notEnoughPositionException =
+                assertThrows(NotEnoughPositionException.class, () -> parkingBoy.park(new Car()));
+
+        //then
+        assertEquals("Not Enough Position", notEnoughPositionException.getMessage());
+    }
+
+    @Test
+    public void should_throw_unrecognized_parking_ticket_exception_when_fetch_given_a_used_ticket_and_multiple_parking_lots_that_the_car_is_fetched() throws Exception {
+        //given
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(new ParkingLot(1));
+        parkingLots.add(new ParkingLot(5));
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
+        Ticket ticket = parkingBoy.park(new Car());
+        parkingBoy.fetch(ticket);
+
+        //when
+        final UnrecognizedParkingTicketException unrecognizedParkingTicketException =
+                assertThrows(UnrecognizedParkingTicketException.class, () -> parkingBoy.fetch(ticket));
+
+        //then
+        assertEquals("Unrecognized Parking Ticket", unrecognizedParkingTicketException.getMessage());
+    }
+
+    @Test
+    public void should_throw_unrecognized_parking_ticket_exception_when_fetch_given_a_fake_ticket_and_multiple_parking_lots_with_car_parked() throws NotEnoughPositionException {
+        //given
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(new ParkingLot(1));
+        parkingLots.add(new ParkingLot(5));
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
+        parkingBoy.park(new Car());
+        Ticket fakeTicket = new Ticket();
+        //when
+        final UnrecognizedParkingTicketException unrecognizedParkingTicketException =
+                assertThrows(UnrecognizedParkingTicketException.class, () -> parkingBoy.fetch(fakeTicket));
+
+        //then
+        assertEquals("Unrecognized Parking Ticket", unrecognizedParkingTicketException.getMessage());
+    }
 }
