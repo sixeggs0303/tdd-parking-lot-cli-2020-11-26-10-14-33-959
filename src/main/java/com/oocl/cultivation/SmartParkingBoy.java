206a22b7723f6983236bcc16b529c0adbcdd2677
Marcus Luk <marcus.luk@oocl.com>
2020-11-27 18:37:46 +0800

    [SmartParkingBoy][Marcus Luk] Test: New test case for parking a car to parking lot with more empty position.

---
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -0,0 +1,9 @@
+package com.oocl.cultivation;
+
+import java.util.List;
+
+public class SmartParkingBoy extends ParkingBoy{
+    public SmartParkingBoy(List<ParkingLot> parkingLotList) {
+        super(parkingLotList);
+    }
+}
---
+++ src/test/java/com/oocl/cultivation/SmartParkingBoyTest.java
@@ -0,0 +1,30 @@
+package com.oocl.cultivation;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SmartParkingBoyTest {
+    @Test
+    public void should_be_parked_to_parking_lot_2_when_park_a_car_given_parking_lot_2_has_more_empty_position() throws Exception {
+        //given
+        ParkingLot parkingLot1 = new ParkingLot(1);
+        ParkingLot parkingLot2 = new ParkingLot(10);
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        Car car = new Car();
+
+        //when
+        Ticket ticket = smartParkingBoy.park(new Car());
+
+        //then
+        assertEquals(car, parkingLot2.fetch(ticket));
+    }
+
+}
